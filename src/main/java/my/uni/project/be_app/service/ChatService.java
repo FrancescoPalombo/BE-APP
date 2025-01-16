6c4f995f0578d05616482080a1f780d635752140
FrancescoPalombo <francesco.palombo@edu.unife.it>
2025-01-16 18:29:51 +0100

    Update ChatService.java

--- src/main/java/my/uni/project/be_app/service/ChatService.java
+++ src/main/java/my/uni/project/be_app/service/ChatService.java
@@ -9,7 +9,7 @@ import my.uni.project.be_app.model.Message;
 import my.uni.project.be_app.model.WordsDataset;
 
 @Service
-public class ChatService {
+public class ChatService {    //Servizio Gestione Messaggi Ricevuti dal Controllore
     
     private List<Message> messages = new ArrayList<>();
 
@@ -21,24 +21,26 @@ public class ChatService {
         this.n=this.bannedWords.getLength();
     }
 
-    public List<Message> getMessages(){
+    public List<Message> getMessages(){    //Metodo Get Cronologia Messaggi Inviati
         return messages;
     }
 
-    public void setMessage(Message message){
-        if (message == null || message.getMessage() == null) {
+    public void setMessage(Message message){    //Metodo iniziale per Moderazione Messaggio
+        if (message == null || message.getMessage() == null) {    //Gestione errore in caso di messaggio vuoto
             System.out.println("Messaggio nullo, restituito messaggio vuoto.");
         }
         else{
+            //Imposto le variabili per gestire la moderazione del Messaggio
             boolean changed=false;
             String[] words = message.getMessage().split(" ");
             int size = words.length;
             boolean found;
+            //ciclo for per ogni parola del messaggio, usa Metodo di ricerca binaria per confrontarlo col Dataset
             for(int i=0; i<size; i++){
                 found = binarySearch(words[i]);
                 if(found){words[i]="***"; changed=true;}
             }
-            Message newMessage = new Message("");
+            Message newMessage = new Message(""); //Creo un oggetto Messaggio per salvare il messaggio ricevuto Moderato o meno
             if(changed){
                 String moderatedMessage = String.join(" ", words);
                 newMessage.setMessage(moderatedMessage);
@@ -47,11 +49,11 @@ public class ChatService {
             else{
                 newMessage.setMessage(message.getMessage());
             }
-            messages.add(newMessage);
+            messages.add(newMessage); //Aggiungo  il messaggio alla cronologia dei messaggi ricevuti
         }
     }
 
-    private boolean binarySearch(String word){
+    private boolean binarySearch(String word){    //Metodo ricerca binaria sul dataset data una parola del messaggio
         int first=0, last=n-1, med=(first+last)/2;
         boolean found=false;
         while((first<=last)&&(!found)){
